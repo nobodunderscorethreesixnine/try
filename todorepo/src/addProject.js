1e7ad3cbf3df071385770cf2ed450a84b16f28ff
nobodunderscorethreesixnine <113294742+nobodunderscorethreesixnine@users.noreply.github.com.>
2025-03-02 18:25:34 +0545

    sharing for universe

---
+++ todorepo/src/addProject.js
@@ -0,0 +1,34 @@
+// const prjList = document.querySelector('.prj-list')
+// const prjInput = document.querySelector('#prj-input')
+// function createPrjDom(prjName){
+//     const p = document.createElement('p')
+//     p.classList.add('prj-name')
+//     p.textContent = prjName
+//     prjList.appendChild(p)
+// }
+
+// export default class AddProject{
+//     constructor(input){
+//         this.input = input;
+//         prjInput.style.visibility = 'visible'
+//         createPrjDom(this.input)
+//     }
+
+// }
+
+// new AddProject('name')
+
+
+// if user click + then
+// open input field
+// after opening input field 
+// if user again click + then
+// run reset form and func focusOut
+
+// else if user input.length > 0 and user press enter then
+// append in prj list
+// else 
+// reset form and run func focusOut;
+
+// now place text in ce then close without executing other func.nter, before checking all this make sure
+// if user press + again
