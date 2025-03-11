c1a78c2ef0dfd537d656fc1300af22827d4cfada
nobodunderscorethreesixnine <113294742+nobodunderscorethreesixnine@users.noreply.github.com.>
2025-03-11 09:19:00 +0545

    sharing for mac

--- todorepo/src/addProject.js
+++ todorepo/src/addProject.js
@@ -1,5 +1,19 @@
-export default class AddProject {
-    constructor() {
-        
-    }
-}
+// export default class AddProject {
+//     constructor(prjInput,projectNames) {
+//         this.prjInput = prjInput
+//         this.projectNames = projectNames
+//         prjNameLength = prjInput.length
+//     }
+
+//     showInputBox() {
+//         // console.log('it also works')
+//         this.prjInput.style.visibility = 'visible'
+//         this.prjInput.focus()
+//     }
+
+ 
+
+// }
+
+
+
